7c037f01589a8b599358a4e744cfafd6b00de4fc
andrew <andrew91@mail.ru>
2022-11-26 10:29:28 +0300

    first commit

---
+++ src/componets/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+    <header className='header'>
+        <img src='https://i.pinimg.com/75x75_RS/30/3b/e0/303be0e2178fd98e791b339915d03d81.jpg'></img>
+    </header>);
+}
+
+export default Header;
---
+++ src/componets/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <div>
+          <img src="https://pbs.twimg.com/media/CaXKxXTUEAAdG_m.jpg"></img>
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          my post
+          <div>
+            new post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </div>);
+}
+
+export default Profile;
